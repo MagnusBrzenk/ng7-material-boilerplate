43845e25d8d38f9ef545a2147d9679ae9592c07e
MagnusBrzenk <magnusbrzenk@gmail.com>
2019-05-09 12:30:39 -0400

    Effective toy example on footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,12 +9,120 @@ import { routeChangeTrigger } from './core/animations/route-change.animations';
 import { appLoadingTrigger } from './core/animations/app-loading.animations';
 import { AnimationsService } from './core/animations/animations.service';
 import { IAnimEvent } from './core/animations/animations.models';
+import {
+  trigger,
+  query,
+  style,
+  animate,
+  transition,
+  state,
+  sequence,
+  group
+} from '@angular/animations';
+
+export function blah() {
+  return true;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  animations: [routeChangeTrigger, appLoadingTrigger]
+  animations: [
+    routeChangeTrigger,
+    appLoadingTrigger,
+    trigger('testSafari', [
+      // state('start', style({ backgroundColor: 'blue', marginTop: 60 })),
+      // state('finish', style({ backgroundColor: 'red', marginTop: 0 })),
+      // transition('start <=> finish', animate('500ms ease-in-out')),
+      transition('* <=> *', [
+        //////////////////////////////////
+        sequence([
+          query(
+            '.footer-wrapper',
+            [
+              style({
+                backgroundColor: 'blue'
+                // position: 'absolute'
+              })
+            ],
+            {
+              optional: true
+            }
+          ),
+          query(
+            '.footer-wrapper',
+            [
+              style({
+                // backgroundColor: 'blue'
+              }),
+              animate(
+                '500ms ease-in-out',
+                style({
+                  //
+                  // marginTop: 60
+                  minHeight: 0,
+                  maxHeight: 0,
+                  height: 0
+                  // position: 'absolute'
+                })
+              )
+            ],
+            {
+              optional: true
+            }
+          ),
+          group([
+            query(
+              '.footer-wrapper',
+              [
+                style({
+                  //
+                  // marginTop: 60
+                  // minHeight: 0,
+                  // height: 0,
+                  // position: 'absolute'
+                }),
+                animate(
+                  '.5s ease-in-out',
+                  style({
+                    height: 60,
+                    minHeight: 60,
+                    maxHeight: 60
+                    // marginTop: 0
+                  })
+                )
+              ],
+              {
+                optional: true
+              }
+            ),
+            query(
+              '.footer-wrapper',
+              [
+                style({
+                  //
+                  // marginTop: 60
+                  // minHeight: 0,
+                  // height: 0,
+                  // position: 'absolute'
+                }),
+                animate(
+                  '.5s ease-in-out',
+                  style({
+                    backgroundColor: 'green'
+                  })
+                )
+              ],
+              {
+                optional: true
+              }
+            )
+          ])
+        ])
+      ])
+    ])
+  ]
 })
 export class AppComponent implements OnInit {
   ///////////////////////////////////////////
@@ -22,6 +130,8 @@ export class AppComponent implements OnInit {
   logo = require('../assets/icons/icon-72x72.png');
   localStorageState$: Observable<ILocalStorageState>;
   theme$: Observable<string>;
+
+  isCLicked = false;
 
   constructor(private route: Router, private localStorageService: LocalStorageService) {
     //
@@ -34,6 +144,12 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit(): void {}
+
+  testClick() {
+    this.isCLicked = !this.isCLicked;
+
+    console.log('CLICKED ', this.isCLicked);
+  }
 
   routeChangeState(outletEvent: RouterOutlet) {
     const result =
--- src/app/core/animations/route-change.animations.ts
+++ src/app/core/animations/route-change.animations.ts
@@ -32,39 +32,44 @@ export const routeChangeTrigger = trigger('routeChangeAnimations', [
     [
       sequence([
         // First, shrink footer
-        query(
-          '.footer-wrapper',
-          [
-            style({
-              //
-              overflow: 'hidden'
-            }),
-            animate(
-              '0.5s ease-in-out',
-              style({
-                //
-                marginTop: footerHeightPxls
-                // height: 0
-                // minHeight: 0
-              })
-            )
-          ],
-          {
-            optional: true
-          }
-        ),
+        // query(
+        //   '.footer-wrapper',
+        //   [
+        //     style({
+        //       //
+        //       overflow: 'hidden'
+        //     }),
+        //     animate(
+        //       '10.5s ease-in-out',
+        //       style({
+        //         //
+        //         marginTop: footerHeightPxls
+        //         // height: 0
+        //         // minHeight: 0
+        //       })
+        //     )
+        //   ],
+        //   {
+        //     optional: true
+        //   }
+        // ),
 
         ////
         // Shift new page out of sight
         query(
-          ':enter, :leave',
+          ':enter',
           [
             style({
-              opacity: 0,
-              position: 'absolute',
-              transform: 'translateX(-200%)'
+              // opacity: 0,
+              // position: 'fixed',
+              // transform: 'translateX(-200%)'
             }),
-            animate(1)
+            animate(1),
+            style({
+              opacity: 0,
+              position: 'fixed',
+              transform: 'translateX(-200%)'
+            })
           ],
           {
             optional: true
@@ -85,15 +90,43 @@ export const routeChangeTrigger = trigger('routeChangeAnimations', [
         ////
 
         group([
-          /////////////////
-
-          /////////////////
           query(
             ':leave',
             [
               style({ transform: 'translateY(0%)', opacity: 1 }),
-              animate('0.5s ease-in-out', style({ transform: 'translateY(3%)', opacity: 0 })),
-              style({ position: 'relative', display: 'none' })
+              animate('1.51s ease-in-out', style({ transform: 'translateY(3%)', opacity: 0 })),
+              style({
+                //
+                display: 'none'
+              })
+            ],
+            { optional: true }
+          ),
+          query(
+            '.footer-wrapper',
+            [
+              style({
+                //
+                opacity: 1
+                // height: 60,
+                // minHeight: 60,
+                // maxHeight: 60
+              }),
+              animate(
+                '1.51s ease-in-out',
+                style({
+                  //
+                  opacity: 0
+                  // height: 0,
+                  // minHeight: 0,
+                  // maxHeight: 0
+                })
+              ),
+              style({
+                //
+                // position: 'relative',
+                // display: 'none'
+              })
             ],
             { optional: true }
           )
@@ -106,7 +139,8 @@ export const routeChangeTrigger = trigger('routeChangeAnimations', [
               style({
                 //
                 transform: 'translate(0%, 3%)',
-                position: 'relative'
+                position: 'relative',
+                opacity: 0.5
               }),
               animate(
                 '0.5s ease-in-out',
@@ -119,44 +153,72 @@ export const routeChangeTrigger = trigger('routeChangeAnimations', [
               )
             ],
             { optional: true }
+          ),
+          query(
+            '.footer-wrapper',
+            [
+              style({
+                //
+                opacity: 0.9
+                // height: 0,
+                // minHeight: 0,
+                // maxHeight: 0
+              }),
+              animate(
+                '0.5s ease-in-out',
+                style({
+                  //
+                  opacity: 1
+                  // height: 60,
+                  // minHeight: 60,
+                  // maxHeight: 60
+                })
+              ),
+              style({
+                //
+                // position: 'relative',
+                // display: 'none'
+              })
+            ],
+            { optional: true }
           )
-        ]),
+        ])
         // Restore footer:
 
-        query(
-          '.footer-wrapper',
-          [
-            // style({
-            //   //
-            //   overflow: 'hidden'
-            // }),
-            animate(
-              '0.5s ease-in-out',
-              style({
-                marginTop: 0
-                // height: footerHeightPxls
-              })
-            )
-          ],
-          {
-            optional: true
-          }
-        ),
+        // query(
+        //   '.footer-wrapper',
+        //   [
+        //     // style({
+        //     //   //
+        //     //   overflow: 'hidden'
+        //     // }),
+        //     animate(
+        //       '0.5s ease-in-out',
+        //       style({
+        //         marginTop: 0
+        //         // height: footerHeightPxls
+        //       })
+        //     )
+        //   ],
+        //   {
+        //     optional: true
+        //   }
+        // ),
 
         // Animate targeted elements within new page into view
-        query(
-          ':enter .' + ROUTE_ANIMATIONS_ELEMENTS,
-          [
-            stagger('0.15s ease-in-out', [
-              style({ transform: 'translate(0%, 3%)', opacity: 0 }),
-              animate(
-                '0.15s ease-in-out',
-                style({ transform: 'translateY(0%)', opacity: 1, position: 'relative' })
-              )
-            ])
-          ],
-          { optional: true }
-        )
+        // query(
+        //   ':enter .' + ROUTE_ANIMATIONS_ELEMENTS,
+        //   [
+        //     stagger('0.15s ease-in-out', [
+        //       style({ transform: 'translate(0%, 3%)', opacity: 0 }),
+        //       animate(
+        //         '0.15s ease-in-out',
+        //         style({ transform: 'translateY(0%)', opacity: 1, position: 'relative' })
+        //       )
+        //     ])
+        //   ],
+        //   { optional: true }
+        // )
       ]),
       query(
         ':enter',
