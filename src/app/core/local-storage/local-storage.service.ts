b36bed8e3709b572ab429467a66a91a49e8757b1
MagnusBrzenk <magnusbrzenk@gmail.com>
2019-05-20 18:43:58 -0400

    Fix localstorage and theming

--- src/app/core/local-storage/local-storage.service.ts
+++ src/app/core/local-storage/local-storage.service.ts
@@ -1,6 +1,7 @@
-import { Injectable } from '@angular/core';
-import { permittedThemes, ILocalStorageState } from './local-storage.models';
 import { Subject } from 'rxjs';
+import { Injectable } from '@angular/core';
+import { OverlayContainer } from '@angular/cdk/overlay';
+import { permittedThemes, ILocalStorageState } from './local-storage.models';
 
 type LSKey = keyof ILocalStorageState; // Define type for 'LocalStorageKeys'
 
@@ -15,7 +16,7 @@ export class LocalStorageService {
 
   private state$ = new Subject<ILocalStorageState>();
 
-  constructor() {
+  constructor(private overlayContainer: OverlayContainer) {
     this.verifyAndRepairLocalStorageState();
     this.state$.next({ ...this.getLocalStorageState() });
   }
@@ -41,13 +42,14 @@ export class LocalStorageService {
   setItem(key: LSKey, value: any) {
     localStorage.setItem(key, JSON.stringify(value));
     this.refreshStateStream();
+    if (key === 'SiteTheme') this.updateCdkOverlayClass(value);
   }
 
   getItem(key: LSKey) {
     // Use default values for testing
     if (!localStorage) return this.initialRequiredState[key];
 
-    const item = localStorage.getItem(`${key}`);
+    const item = localStorage.getItem(key);
 
     try {
       return !!item ? JSON.parse(item) : 'NO_ITEM_FOUND';
@@ -58,54 +60,37 @@ export class LocalStorageService {
   }
 
   removeItem(key: string) {
-    localStorage.removeItem(`${key}`);
+    localStorage.removeItem(key);
   }
 
-  // Tests that localStorage exists, can be written to, and read from
-  testLocalStorage() {
-    const testValue = 'testValue';
-    const testKey = 'testKey';
-    let retrievedValue: string;
-    const errorMessage = 'localStorage did not return expected value';
-
-    this.setItem(testKey, testValue);
-    retrievedValue = this.getItem(testKey);
-    this.removeItem(testKey);
-
-    if (retrievedValue !== testValue) {
-      throw new Error(errorMessage);
-    }
-  }
-
-  //
-  // Check that local storage ONLY has "required" key-value pairs.
-  // Remove any pairs that are not "required".
-  // Create and apply default value to any missing pairs.
-  //
+  /**
+   * Check that local storage ONLY has "required" key-value pairs.
+   * Remove any pairs that are not "required".
+   * Create and apply default value to any missing pairs.
+   */
   verifyAndRepairLocalStorageState() {
     //
     // Get state objects
-    //
     const reqState: ILocalStorageState = this.initialRequiredState;
     const state: any = this.getLocalStorageState();
 
-    //
+    // Loop through keys of state; remove pair if key is not in reqState
+    Object.keys(state).forEach((stateKey: string) => {
+      if (!Object.keys(reqState).includes(stateKey)) this.removeItem(stateKey);
+    });
+
     // Verify key-value pairs and types by looping thru keys of requiredInitialState;
     // if present state has key missing or of wrong type, then reinitialize it
-    //
     const newState: ILocalStorageState = (Object.keys(reqState) as LSKey[]).reduce(
       (accumState: any, key: LSKey) => {
         //
         // Build tests that compare key-value pairs between
         // present state and initial-required state
-        //
         const isKeyAbsent = !state[key];
         const isBasicTypeWrong = typeof state[key] !== typeof (reqState[key] as any);
         const isThemeTypeWrong = !permittedThemes.includes(state[key]);
 
-        //
         // If problem is found with key-value pair, reset it
-        //
         if (isKeyAbsent || isBasicTypeWrong || isThemeTypeWrong) {
           accumState[key] = reqState[key];
         } else {
@@ -120,5 +105,19 @@ export class LocalStorageService {
     (Object.keys(newState) as LSKey[]).forEach((key: LSKey) => {
       this.setItem(key, newState[key]);
     });
+  }
+
+  /**
+   * Method to be triggered when SiteTheme is updated to update div of class
+   * 'cdk-overlay-container' to also be of class equal to the theme's name
+   */
+  updateCdkOverlayClass(newTheme: string) {
+    //
+    const classList: DOMTokenList = this.overlayContainer.getContainerElement().classList;
+    const toRemove = Array.from(classList).filter((item: string) => item.includes('-theme'));
+    if (toRemove.length) {
+      classList.remove(...toRemove);
+    }
+    classList.add(newTheme.toLowerCase());
   }
 }
